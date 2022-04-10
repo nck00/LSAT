3f10e7d2b854bebf4cbb9c7db34eb945ff922640
nck00 <50334352+nck00@users.noreply.github.com>
2022-04-10 19:10:21 +0200

    Removed prints

--- core/widgets/Catalog/catalog_main.py
+++ core/widgets/Catalog/catalog_main.py
@@ -36,9 +36,7 @@ class Catalog(QDockWidget):
         self.model = QFileSystemModel()
         self.view = TreeView()  # QTreeView with SizeHint
         self.view.setStyleSheet("QHeaderView::section { background-color:#b7cbeb }")
-        print("a")
         # self.view.header().setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
-        print("b")
         self.setWidget(self.view)
         self.message = Messenger()
 
